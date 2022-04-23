893808f8d27ae4a690e8543670350a8fd0ce88b5
Cauã Melo <cauamelo2345@gmail.com>
2022-04-23 13:47:51 -0300

    add PlayerDamageEvent

---
+++ src/main/java/br/com/speedmc/listeners/PlayerDamageEvent.java
@@ -0,0 +1,20 @@
+package br.com.speedmc.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import static br.com.speedmc.SpeedRankUP.*;
+
+public class PlayerDamageEvent implements Listener {
+
+    // Cancel damage on world cidade
+    @EventHandler
+    public void onDamage(EntityDamageEvent e) {
+        if (e.getEntity().getType() != EntityType.PLAYER) return;
+        if (e.getEntity().getWorld() != Bukkit.getWorld(getInstance().getConfig().getString("MundoCidade"))) return;
+        e.setCancelled(true);
+    }
+
+}
